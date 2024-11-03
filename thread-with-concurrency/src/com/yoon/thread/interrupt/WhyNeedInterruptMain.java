df6a68ab36b10d619e71dd61f5490377b8e1cb7d
6161990 <6161990src@gmail.com>
2024-11-03 16:55:39 +0900

    설명 보강

--- thread-with-concurrency/src/com/yoon/thread/interrupt/WhyNeedInterruptMain.java
+++ thread-with-concurrency/src/com/yoon/thread/interrupt/WhyNeedInterruptMain.java
@@ -13,7 +13,7 @@ public class WhyNeedInterruptMain {
         thread.start();
         sleep(4000);
         log("interrupt working --------- 작업 중단 지시 ");
-        thread.interrupt(); // 바로 예외가 터지는 건 아니고 내부에서 Join or Sleep 메소드처럼 InterruptedException 을 핸들링하고 있으면 예외 발생함
+        thread.interrupt();  // interrupt 호출 시 바로 예외가 발생하지 않고 sleep, wait, join 등의 메서드가 실행 중일 때 InterruptedException 발생
         log("isInterrupted?" + thread.isInterrupted());
     }
 
@@ -27,10 +27,10 @@ public class WhyNeedInterruptMain {
                     Thread.sleep(3000); // TIMED_WAITING -> interrupt 에 의해 인터럽트 상태가 되었고 예외 발생
                 }
             } catch (InterruptedException e) {
-                log("isInterrupted? " + Thread.currentThread().isInterrupted()); // Interrupted 에 의해 예외가 발생하면 더이상 Interrupted 가 아니게 됨 false
-                log("인터럽트 상태에서 인터럽트 예외가 발생하면 work 스레드는 다시 동작하는 상태가되면서 interrupted 종료됨"); // Interrupted 에 의해 예외가 발생하면 더이상 Interrupted 가 아니게 됨 false
+                log("isInterrupted? " + Thread.currentThread().isInterrupted()); // Interrupted 에 의해 InterruptedException이 발생
+                log("인터럽트 상태에서 인터럽트 예외가 발생하면 work 스레드는 다시 동작하는 상태가되면서 interrupted 종료됨");
                 log("exception message is " + e.getMessage()); // RUNNABLE
-                log("now state? " + Thread.currentThread().getState());
+                log("now state? " + Thread.currentThread().getState()); // InterruptedException이 발생하면 interrupted 상태는 해제됨 (false)
             }
         }
     }
