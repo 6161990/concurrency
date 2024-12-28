92064f6a8b4b5a828a57b4ae4dae186ae1c1926b
6161990 <147702983+src6161990@users.noreply.github.com>
2024-12-28 15:45:34 +0900

    Yield

--- thread-with-concurrency/src/com/yoon/thread/yield/YieldMain.java
+++ thread-with-concurrency/src/com/yoon/thread/yield/YieldMain.java
@@ -3,6 +3,14 @@ package com.yoon.thread.yield;
 import static com.yoon.utils.LoggerLogger.log;
 import static com.yoon.utils.ThreadSleepUtils.sleep;
 
+/**
+ * Yield
+ * 어떤 스레드가 다른 스레드 먼저 실행하게끔 양보하는 역할
+ * 양보는 보장되지않는다. 완전 완벽으로 동작되는 것은 아님. CPU 에게 힌트만 주는 느낌인 것 같다.
+ * 그래도 상태는 RUNNABLE(READY --> 스케쥴링 큐에서 대기중)
+
+ * sleep 도 비슷하게 흘러가긴한데, TIME_WAITING 상태로 변경된다.
+ * */
 public class YieldMain {
 
     private static final int THREAD_COUNT = 1000;
